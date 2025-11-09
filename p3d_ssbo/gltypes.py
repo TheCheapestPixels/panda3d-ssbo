e36f98a97cc45bb168a7485ec99ec9857eb92832
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2025-11-09 06:44:19 +0100

    Packing, unpacking, GLSL creation... A lot of stuff works now.

--- p3d_ssbo/gltypes.py
+++ p3d_ssbo/gltypes.py
@@ -35,19 +35,41 @@
 # The workhorse of this library is the class `GlType`, on which
 # everything else is built.
 #
-# * `size`
-#   * `_size`
-# * `glsl`
-# * `pack`: User-facing API that basically just calls...
-#   * `_pack`: Return updated buffer and trailing padding by...
-#     * `_pad_to_alignment`: Align to beginning of this/these elemet(s).
-#     * `_add_element`: Return buffer with added data from...
+# * `glsl`: Returns the element's GLSL representation. Called at the top
+#   level element, this returns the whole definition that can be dropped
+#   into shaders.
+# * `size`: Wrapper for `_size`, returns length in bytes, to be called
+#   at the top level. Syntactic candy, like `pack` and `unpack`.
+#   * `_size`: (buffer size, trailing) -> (new size, new trailing)
+# * `pack`: py_data -> byte_data.
+#   * `_pack`: (py_data, byte_data, rest_dims, trailing) ->
+#              (new byte_data, new trailing)
+#     There are two possible cases:
+#     1) This element is *not* an array. Pad to trailing alignment,
+#        `_add_element`, return the updated buffer and the new trailing.
+#     2) It *is* an array. Pad to trailing alignment, then call `_pack`
+#        recursively (sidestepping the padding to trailing) until
+#        individual elements are reached again.
+#     * `_add_element`: (byte_data, py_data)->(new byte_data, trailing)
 #       * pack_element`: Turn Python data to byte data
-
-
-
-
-
+# * `unpack`: byte_data -> py_data
+#   * `_unpack`: (byte_data, read_at, rest_dims, trailing) ->
+#                (py_data, read_at, trailing)
+#     The converse to `_pack`. Instead of removing data from the buffer,
+#     though, it uses an index into the data (read_at). Otherwise, it
+#     works exactly like `_pack`.
+#     * `_read_element`: (byte_data, read_at)->(py_data, trailing)
+#       * `unpack_element`: Return Python data at read_at
+#
+# Structs are a bit special, FIXME:
+# * There's Type and there's Instance
+# * They overwrite `_add_element` and `_read_element`.
+#
+# Often used variable names
+# * buffer_so_far: Binary buffer content.
+# * size_so_far: Length of the binary buffer.
+# * trailing: Alignment that the following element has (at the least) to
+#   align to.
 
 
 from array import array
@@ -61,95 +83,114 @@ class GlType:
         self.field_name = field_name
         self.dims = dims
 
-    def size(self):
-        size, trailing_offset = self._size()
-        return size * 4
+    def glsl(self):
+        """
+        Return the GLSL string defining the element.
+        """
+        dim_string = ''.join(f'[{d}]' for d in self.dims)
+        return self.glsl_type_name  + ' ' + self.field_name + dim_string + ';'
 
-    def _size(self, size_so_far=0, trailing_offset=0):
-        # Align
-        pad_length = self._calculate_offset(size_so_far, trailing_offset)
-        size_so_far += pad_length
-        # Add element(s)
-        stride = self.element_size + self._calculate_offset(self.element_size)
-        array_factor = math.prod(self.dims)
-        size_so_far += stride * (array_factor - 1) + self.element_size
-        # If this is an array or struct, the next element may require
-        # padding.
-        trailing_offset = 0
-        if self.dims != () or isinstance(self, StructInstance):
-            trailing_offset = self.alignment
-        return size_so_far, trailing_offset
-
-    def _calculate_offset(self, size_so_far=0, trailing_offset=0):
-        alignment = max(self.alignment, trailing_offset)
+    def _calculate_offset(self, size_so_far=0, trailing=0):
+        """
+        Given the size of the buffer so far, and the offset that the
+        previous element requires, how much padding does the buffer
+        require to be properly aligned for this element?
+        """
+        alignment = max(self.alignment, trailing)
         pad_length = 0
         if size_so_far % alignment != 0:
             pad_length = alignment - size_so_far % alignment
         return pad_length
         
-    def glsl(self):
-        dim_string = ''.join(f'[{d}]' for d in self.dims)
-        return self.glsl_type_name  + ' ' + self.field_name + dim_string + ';'
+    def size(self):
+        "Returns this element's data size, without leading of trailing offsets."
+        size, trailing = self._size()
+        return size * 4
 
-    def pack(self, py_data, buffer_so_far=None, rest_dims=None):
-        buffer_data, trailing_offset = self._pack(
+    def _size(self, size_so_far=0, trailing=0):
+        # Align
+        pad_length = self._calculate_offset(size_so_far, trailing)
+        size_so_far += pad_length
+        # Add element(s)
+        stride = self.element_size + self._calculate_offset(self.element_size)
+        array_factor = math.prod(self.dims)
+        size_so_far += stride * (array_factor - 1) + self.element_size
+        # FIXME: If this is an array or struct, the next element may
+        # require padding.
+        trailing = 0
+        if self.dims != () or isinstance(self, StructInstance):
+            trailing = self.alignment
+        return size_so_far, trailing
+
+    def pack(self, py_data):
+        "Turn this Python data structure into buffer byte data."
+        byte_data, trailing = self._pack(
             py_data,
             b''
         )
-        return buffer_data
+        return byte_data
 
-    def _pack(self, py_data, buffer_so_far, rest_dims=None, trailing_offset=0):
+    def _pack(self, py_data, byte_data, rest_dims=None, trailing=0):
         if rest_dims is None:
             rest_dims = self.dims
-            buffer_so_far = self._pad_to_alignment(buffer_so_far, trailing_offset)
+            byte_data = self._pad_to_alignment(byte_data, trailing)
         # Add data
         if rest_dims == ():  # Individual element
-            buffer_so_far = self._pad_to_alignment(buffer_so_far)
-            buffer_so_far, next_trailing_offset = self._add_element(buffer_so_far, py_data)
+            byte_data = self._pad_to_alignment(byte_data)
+            byte_data, next_trailing = self._add_element(byte_data, py_data)
         else:  # Array
             assert len(py_data) == self.dims[0], f"{len(py_data)} elements given for a size {self.dims[0]} array."
             for idx, py_data_piece in enumerate(py_data):
-                buffer_so_far, trailing_offset = self._pack(
+                byte_data, trailing = self._pack(
                     py_data_piece,
-                    buffer_so_far,
+                    byte_data,
                     rest_dims[1:],
                 )
-            next_trailing_offset = self.alignment
-        return buffer_so_far, next_trailing_offset
+            next_trailing = self.alignment
+        return byte_data, next_trailing
 
-    def _pad_to_alignment(self, buffer_so_far, trailing_offset=0):
-        assert len(buffer_so_far) % 4 == 0
-        pad_length = self._calculate_offset(len(buffer_so_far) // 4, trailing_offset)
-        buffer_so_far += b'\x00' * pad_length * 4
-        return buffer_so_far
+    def _pad_to_alignment(self, byte_data, trailing=0):
+        assert len(byte_data) % 4 == 0
+        pad_length = self._calculate_offset(len(byte_data) // 4, trailing)
+        byte_data += b'\x00' * pad_length * 4
+        return byte_data
 
-    def _add_element(self, buffer_so_far, py_data):
-        buffer_so_far += self.pack_element(py_data)
-        trailing_offset = 0
-        return buffer_so_far, trailing_offset
+    def _add_element(self, byte_data, py_data):
+        byte_data += self.pack_element(py_data)
+        trailing = 0
+        return byte_data, trailing
 
-    def unpack(self, byte_data, read_at=0, rest_dims=None):
-        # FIXME: Trailing offset
+    def unpack(self, byte_data):
+        py_data, read_at, trailing = self._unpack(byte_data)
+        return py_data
+
+    def _unpack(self, byte_data, read_at=0, rest_dims=None, trailing=0):
+        # Align to trailing offset
         if rest_dims is None:
             rest_dims = self.dims
-        # Align
-        if read_at % self.alignment != 0:
-            offset = self.alignment - read_at % self.alignment
-            read_at += offset
+            read_at += self._calculate_offset(read_at, trailing)
         # Unpack_data
         if rest_dims == ():
-            py_data = self.unpack_element(byte_data, read_at)
+            read_at += self._calculate_offset(read_at)
+            py_data, trailing = self._read_element(byte_data, read_at)
+            read_at += self.element_size
         else:
             stride = self.element_size * math.prod(rest_dims[1:])
             py_data = tuple(
-                self.unpack(
+                self._unpack(
                     byte_data,
                     read_at + index * stride,
                     rest_dims[1:],
-                )
+                )[0]
                 for index in range(rest_dims[0])
             )
-        return py_data
+            trailing = self.alignment
+        return py_data, read_at, trailing
+
+    def _read_element(self, byte_data, read_at):
+        py_data = self.unpack_element(byte_data, read_at)
+        trailing = 0  # FIXME?
+        return py_data, trailing
 
 
 class GlFloat(GlType):
@@ -198,7 +239,6 @@ class GlVec3(GlType):
 
 class Struct:
     def __init__(self, type_name, *fields):
-        self.type_name = type_name
         self.fields = fields
         self.glsl_type_name = type_name
         self.alignment = max(f.alignment for f in fields)
@@ -208,11 +248,24 @@ class Struct:
         return instance
 
     def glsl(self):
-        text = f"struct {self.type_name} {{\n"
+        text = f"struct {self.glsl_type_name} {{\n"
         for field in self.fields:
             text += f"  {field.glsl()}\n"
         text += "}"
         return text
+
+    def _get_struct_types(self, types=None):
+        if types is None:
+            types = []
+        if self not in types:
+            structs = [
+                f for f in self.fields
+                if isinstance(f, StructInstance)
+            ]
+            for field in structs:
+                types = field._get_struct_types(types)
+            types.append(self)
+        return types
 
 
 class StructInstance(GlType):
@@ -229,13 +282,101 @@ class StructInstance(GlType):
 
     def glsl(self):
         dim_string = ''.join(f'[{d}]' for d in self.dims)
-        text = f"{self.type_obj.type_name} {self.field_name}{dim_string};"
+        text = f"{self.type_obj.glsl_type_name} {self.field_name}{dim_string};"
         return text
 
-    def _add_element(self, buffer_so_far, py_data):
+    def _add_element(self, byte_data, py_data):
         for field, data in zip(self.type_obj.fields, py_data):
-            buffer_so_far, trailing_offset = field._pack(data, buffer_so_far)
-        return buffer_so_far, self.alignment
+            byte_data, trailing = field._pack(data, byte_data)
+        return byte_data, self.alignment
 
-    def unpack_element(self, byte_data, read_at):
-        pass
+    def _read_element(self, byte_data, read_at):
+        struct_py_data = []
+        for field in self.type_obj.fields:          
+            py_data, read_at, trailing = field._unpack(byte_data, read_at)
+            struct_py_data.append(py_data)
+        return tuple(struct_py_data), self.alignment
+
+    def _get_struct_types(self, types=None):
+        if types is None:
+            types = []
+        if self.type_obj not in types:
+            types = self.type_obj._get_struct_types(types)
+        return types
+
+
+class Buffer:
+    def __init__(self, type_name, *fields):
+        self.fields = fields
+        self.glsl_type_name = type_name
+        self.alignment = max(f.alignment for f in fields)
+
+    def glsl(self):
+        text = f"layout(std430) buffer {self.glsl_type_name} {{\n"
+        for field in self.fields:
+            text += f"  {field.glsl()}\n"
+        text += "}"
+        return text
+
+    def _add_element(self, byte_data, py_data):
+        for field, data in zip(self.fields, py_data):
+            byte_data, trailing = field._pack(data, byte_data)
+        return byte_data, self.alignment
+
+    def _read_element(self, byte_data, read_at):
+        struct_py_data = []
+        for field in self.fields:          
+            py_data, read_at, trailing = field._unpack(byte_data, read_at)
+            struct_py_data.append(py_data)
+        return tuple(struct_py_data), self.alignment
+
+    def _get_struct_types(self, types=None):
+        if types is None:
+            types = []
+        structs = [
+            f
+            for f in self.fields
+            if isinstance(f, StructInstance)
+        ]
+        for field in structs:
+            types = field._get_struct_types(types)
+        return types
+
+    def _get_buffers(self):
+        return [self]
+
+    def full_glsl(self):
+        structs = self._get_struct_types()
+        struct_glsl = '\n\n'.join([s.glsl() for s in structs])
+        buffer_glsl = self.glsl()
+        glsl = '\n\n'.join([struct_glsl, buffer_glsl])
+        return glsl
+
+
+class BufferSet:
+    def __init__(self, *buffers):
+        self.buffers = buffers
+
+    def glsl(self):
+        glsl_types = ''.join(t for typ in self._get_types())
+        glsl_buffers = ''.join(buf.glsl() for buf in self.buffers)
+        glsl = ''.join([glsl_types, glsl_buffers])
+        return glsl
+
+    def _get_struct_types(self, types=None):
+        if types is None:
+            types = []
+        for buf in self.buffers:
+            types = buf._get_struct_types(types)
+        return types
+
+    def _get_buffers(self):
+        return self.buffers
+
+    def full_glsl(self):
+        structs = self._get_struct_types()
+        buffers = self._get_buffers()
+        struct_glsl = '\n\n'.join([s.glsl() for s in structs])
+        buffer_glsl = '\n\n'.join([b.glsl() for b in buffers])
+        glsl = '\n\n'.join([struct_glsl, buffer_glsl])
+        return glsl
--- tests/test_gltypes.py
+++ tests/test_gltypes.py
@@ -3,6 +3,8 @@ from array import array
 from p3d_ssbo.gltypes import GlFloat
 from p3d_ssbo.gltypes import GlVec3
 from p3d_ssbo.gltypes import Struct
+from p3d_ssbo.gltypes import Buffer
+from p3d_ssbo.gltypes import BufferSet
 
 
 def test_glsl_float():
@@ -117,6 +119,16 @@ def test_glsl_struct_instance():
     assert glsl == 'MyStruct fnord;'
 
 
+def test_glsl_struct_instance_array():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('bar'),
+    )
+    my_struct = my_struct_type('fnord', 2)
+    glsl = my_struct.glsl()
+    assert glsl == 'MyStruct fnord[2];'
+
+
 def test_size_struct():
     my_struct_type = Struct(
         'MyStruct',
@@ -144,3 +156,282 @@ def test_size_struct_array():
     )
     my_struct = my_struct_type('fnord', 2)
     assert my_struct.size() == (4+4+4+3) * 4
+
+
+def test_pack_struct():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+        GlVec3('bar'),
+    )
+    my_struct = my_struct_type('fnord')
+    data_buffer = my_struct.pack(
+        (1.5, (0.2, 0.4, 0.6))
+    )
+    expected_data = array('f', [1.5]).tobytes()
+    expected_data += b'\x00' * 12
+    expected_data += array('f', [0.2, 0.4, 0.6]).tobytes()
+    assert data_buffer == expected_data
+
+
+def test_pack_struct_array():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+        GlVec3('bar'),
+    )
+    my_struct = my_struct_type('fnord', 2)
+    data_buffer = my_struct.pack(
+        (
+            (1.5, (0.2, 0.4, 0.6)),
+            (0.5, (0.1, 0.3, 0.5)),
+        )
+    )
+    expected_data = array('f', [1.5]).tobytes()
+    expected_data += b'\x00' * 12
+    expected_data += array('f', [0.2, 0.4, 0.6]).tobytes()
+    expected_data += b'\x00' * 4  # Post-struct padding
+
+    expected_data += array('f', [0.5]).tobytes()
+    expected_data += b'\x00' * 12
+    expected_data += array('f', [0.1, 0.3, 0.5]).tobytes()
+
+    assert data_buffer == expected_data
+
+
+def test_unpack_struct():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+        GlVec3('bar'),
+    )
+    my_struct = my_struct_type('fnord')
+    data_buffer = array('f', [1.5]).tobytes()
+    data_buffer += b'\x00' * 12
+    data_buffer += array('f', [0.25, 0.5, 0.75]).tobytes()
+    decoded_data = my_struct.unpack(data_buffer)
+    expected_data = (1.5, (0.25, 0.5, 0.75))
+    assert decoded_data == expected_data
+
+
+def test_unpack_struct_array():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+        GlVec3('bar'),
+    )
+    my_struct = my_struct_type('fnord', 2)
+    data_buffer = array('f', [1.5]).tobytes()
+    data_buffer += b'\x00' * 12
+    data_buffer += array('f', [0.25, 0.5, 0.75]).tobytes()
+    data_buffer += b'\x00' * 4
+    data_buffer += array('f', [3.0]).tobytes()
+    data_buffer += b'\x00' * 12
+    data_buffer += array('f', [0.125, 0.625, 0.875]).tobytes()
+    decoded_data = my_struct.unpack(data_buffer)
+    expected_data = (
+        (1.5, (0.25, 0.5, 0.75)),
+        (3.0, (0.125, 0.625, 0.875)),
+    )
+    assert decoded_data == expected_data
+
+
+def test_get_struct_types_single():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+    )
+    types = my_struct_type._get_struct_types()
+    assert types == [my_struct_type]
+
+    my_struct = my_struct_type('fnord')
+    types = my_struct._get_struct_types()
+    assert types == [my_struct_type]
+
+
+def test_get_struct_types_linear():
+    type_a = Struct(
+        'MyStructA',
+        GlFloat('foo'),
+    )
+    type_b = Struct(
+        'MyStructB',
+        type_a('foo'),
+    )
+
+    types = type_b._get_struct_types()
+    assert types == [type_a, type_b]
+
+    my_struct = type_b('fnord')
+    types = my_struct._get_struct_types()
+    assert types == [type_a, type_b]
+
+
+def test_get_struct_types_tree():
+    type_a = Struct(
+        'MyStructA',
+        GlFloat('foo'),
+    )
+    type_b = Struct(
+        'MyStructB',
+        GlFloat('foo'),
+    )
+    type_c = Struct(
+        'MyStructC',
+        type_a('foo'),
+        type_b('bar'),
+    )
+
+    types = type_c._get_struct_types()
+    assert types == [type_a, type_b, type_c]
+
+    my_struct = type_c('fnord')
+    types = my_struct._get_struct_types()
+    assert types == [type_a, type_b, type_c]
+
+
+def test_get_struct_types_diamond():
+    type_a = Struct(
+        'MyStructA',
+        GlFloat('foo'),
+    )
+    type_b = Struct(
+        'MyStructB',
+        type_a('foo'),
+    )
+    type_c = Struct(
+        'MyStructC',
+        type_a('foo'),
+    )
+    type_d = Struct(
+        'MyStructD',
+        type_b('foo'),
+        type_c('bar'),
+    )
+
+    types = type_d._get_struct_types()
+    assert types == [type_a, type_b, type_c, type_d]
+
+    my_struct = type_d('fnord')
+    types = my_struct._get_struct_types()
+    assert types == [type_a, type_b, type_c, type_d]
+
+
+def test_get_struct_types_buffer_single():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+    )
+    my_buffer = Buffer(
+        'MyBuffer',
+        my_struct_type('foo'),
+    )
+    
+    types = my_buffer._get_struct_types()
+    assert types == [my_struct_type]
+
+
+def test_get_struct_types_buffer_set():
+    my_struct_type = Struct(
+        'MyStruct',
+        GlFloat('foo'),
+    )
+    my_buffer_a = Buffer(
+        'MyBufferA',
+        my_struct_type('foo'),
+    )
+    my_buffer_b = Buffer(
+        'MyBufferB',
+        my_struct_type('foo'),
+    )
+    my_buffer_set = BufferSet(my_buffer_a, my_buffer_b)
+    
+    types = my_buffer_set._get_struct_types()
+    assert types == [my_struct_type]
+
+
+
+recursive_buffer_glsl = """
+struct MyBasicStruct {
+  float foo;
+}
+
+struct LeftDiamond {
+  MyBasicStruct foo;
+}
+
+struct RightDiamond {
+  MyBasicStruct foo;
+}
+
+struct Capstone {
+  LeftDiamond foo;
+  RightDiamond bar;
+}
+
+layout(std430) buffer MyBuffer {
+  Capstone cap;
+}"""[1:]
+
+
+def test_glsl_buffer():
+    basic_type = Struct(
+        'MyBasicStruct',
+        GlFloat('foo'),
+    )
+    left_diamond_type = Struct(
+        'LeftDiamond',
+        basic_type('foo'),
+    )
+    right_diamond_type = Struct(
+        'RightDiamond',
+        basic_type('foo'),
+    )
+    capstone_type = Struct(
+        'Capstone',
+        left_diamond_type('foo'),
+        right_diamond_type('bar'),
+    )
+    my_buffer = Buffer(
+        'MyBuffer',
+        capstone_type('cap')
+    )
+
+    glsl = my_buffer.full_glsl()
+    assert glsl == recursive_buffer_glsl
+
+    my_buffer_set = BufferSet(my_buffer)
+    glsl = my_buffer_set.full_glsl()
+    assert glsl == recursive_buffer_glsl
+
+
+double_buffer_glsl = """
+struct BasicType {
+  float foo;
+}
+
+layout(std430) buffer MyBufferA {
+  BasicType foo;
+}
+
+layout(std430) buffer MyBufferB {
+  BasicType foo;
+}"""[1:]
+
+
+def test_glsl_buffer_set():
+    basic_type = Struct(
+        'BasicType',
+        GlFloat('foo'),
+    )
+    my_buffer_a = Buffer(
+        'MyBufferA',
+        basic_type('foo'),
+    )
+    my_buffer_b = Buffer(
+        'MyBufferB',
+        basic_type('foo'),
+    )
+    my_buffer_set = BufferSet(my_buffer_a, my_buffer_b)
+    glsl = my_buffer_set.full_glsl()
+    assert glsl == double_buffer_glsl
