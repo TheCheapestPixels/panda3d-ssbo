ee4945a84cf48ee87d57503a67be9998ac8151ae
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2025-12-23 03:21:25 +0100

    Moved pivot table construction into spatial hash module.

--- examples/main_boids.py
+++ examples/main_boids.py
@@ -15,6 +15,7 @@ from p3d_ssbo.gltypes import Buffer
 from p3d_ssbo.algos.raw_glsl import RawGLSL
 from p3d_ssbo.algos.random_number_generator import MurmurHash
 from p3d_ssbo.algos.spatial_hash import SpatialHash
+from p3d_ssbo.algos.spatial_hash import PivotTable
 from p3d_ssbo.algos.bitonic_sort import BitonicSort
 from p3d_ssbo.tools.ssbo_particles import SSBOParticles
 
@@ -54,14 +55,12 @@ pivot = Struct(
     GlUInt('start'),
     GlUInt('len'),
 )
-print("Creating data buffer...")
 data_buffer = Buffer(
     'dataBuffer',
     boids('boids', num_elements),
     pivot('pivot', grid_res[0] * grid_res[1] * grid_res[2]),
     #boids('boids', num_elements, unbounded=True),
 )
-print("Data buffer created.")
 
 
 # The compute shaders
@@ -69,77 +68,21 @@ rng = MurmurHash(
     data_buffer,
     ('boids', 'pos'),
     #('boids', 'dir'),
-    debug=True,
 )
 spatial_hash = SpatialHash(
     data_buffer,
     ('boids', 'pos', 'hashIdx'),  # array, position field, hash field
     grid_vol,  # Hash grid volume
     grid_res,  # Hash grid resolution
-    debug=True,
 )
 sorter = BitonicSort(
     data_buffer,
     ('boids', 'hashIdx'),
-    debug=True,
 )
-pivot_start_source = """
-  uint idx = uint(gl_GlobalInvocationID.x);
-  uint hash = boids[idx].hashIdx;
-  uint diff;
-  if (idx == 0) {
-    // This is the first element with the lowest index, and thus we'll
-    // have to write it. So, how much further than the non-existant 
-    // index -1 are we?
-    diff = hash + 1;
-  } else {
-    // A regular element. How many cell indices further are we than the
-    // last boid was? (If we're still in the same cell, that's 0.)
-    uint hashLeft = boids[idx-1].hashIdx;
-    diff = hash - hashLeft;
-  }
-  // If this is such an cell-index-advancing element, we write its boid
-  // index into the pivot table as the start of a run. Since we may have
-  // advanced by *several* cell indices, we'll also need to set the
-  // start of the skipped ones; We set it to this element, so the
-  // runlength for them will be 0.
-  if (diff > 0) {
-    for (uint pivotIdx = hash; pivotIdx > hash - diff; pivotIdx--) {
-      pivot[pivotIdx].start = idx;
-    }
-  }
-  // If this is the last boid, we may need to set the start of any
-  // remaining cell indices, and we set them to "just beyond the end of
-  // the list of boids."
-  if (idx+1 == boids.length()) {
-    for (uint pivotIdx = hash + 1; pivotIdx < pivot.length(); pivotIdx++) {
-      pivot[pivotIdx].start = boids.length();
-    }
-  }
-"""[1:-1]
-pivot_start =  RawGLSL(
+pivot = PivotTable(
     data_buffer,
-    'boids',
-    "",
-    pivot_start_source,
-    debug=True,
-)
-pivot_length_source = """
-  uint pivotIdx = uint(gl_GlobalInvocationID.x);
-  uint start = pivot[pivotIdx].start;
-  uint end;
-  if (pivotIdx == pivot.length() - 1) {
-    end = boids.length();
-  } else {
-    end = pivot[pivotIdx + 1].start;
-  }
-  pivot[pivotIdx].len = end - start;
-"""[1:-1]
-pivot_length =  RawGLSL(
-    data_buffer,
-    'pivot',
-    "",
-    pivot_length_source,
+    ('boids', 'hashIdx'),  # What to build the pivot table for.
+    ('pivot', 'start', 'len'),  # The pivot table, and where it stores start and length.
     debug=True,
 )
 mover_funcs = """
@@ -246,7 +189,6 @@ mover = RawGLSL(
     shader_args=dict(
         radius=0.15,
     ),
-    debug=True,
 )
 movement_actualizer_source = """
   uint idx = gl_GlobalInvocationID.x;
@@ -262,7 +204,9 @@ movement_actualizer = RawGLSL(
 
 # UI
 def set_radius(*args, **kwargs):
-    mover.set_shader_arg('radius', slider_radius['value'])
+    r = slider_radius['value']
+    mover.set_shader_arg('radius', r)
+    print(f"Detection radius: {r}")
 slider_radius = DirectSlider(
     parent=base.a2dTopLeft,
     frameSize=(0, 1, -0.03, 0.03),
@@ -277,13 +221,11 @@ slider_radius = DirectSlider(
 )
 
 
-print("Creating particles...")
 points = SSBOParticles(base.render, data_buffer, ('boids', 'pos'))
-print("Particles created.")
 
 
 def setup_debug():
-    for shader in [rng, spatial_hash, sorter, pivot_start, pivot_length, mover.attach]:
+    for shader in [rng, spatial_hash, sorter, pivot, mover]:
         shader.dispatch()
         data = base.win.gsg.get_engine().extract_shader_buffer_data(
             data_buffer.ssbo,
@@ -294,8 +236,7 @@ def setup_prod():
     stages = [
         ("cmp_spatial_hash", spatial_hash),
         ("cmp_sort_spatial_hashes", sorter),
-        ("cmp_pivot_table_starts", pivot_start),
-        ("cmp_pivot_table_length", pivot_length),
+        ("cmp_pivot_table", pivot),
         ("cmp_mover", mover),
         ("cmp_mover_2", movement_actualizer),
     ]
@@ -303,19 +244,16 @@ def setup_prod():
         bin_mgr.add_bin(bin_name, CullBinManager.BT_fixed, -20+idx)
         shader.attach(points.get_np(), bin_name)
         print(f"Shader {bin_name} attached.")
-print("Shaders defined.")
 setup_prod()
-print("Shaders set up.")
+
 
 #data = base.win.gsg.get_engine().extract_shader_buffer_data(
 #    data_buffer.ssbo,
 #    base.win.gsg,
 #)
 #py_data = data_buffer.unpack(data)
-#import pdb; pdb.set_trace()
 #for element in py_data[0]:
 #    print(element[0])
 
-print("Starting main loop...")
+
 base.run()
-#base.task_mgr.step()
--- p3d_ssbo/algos/spatial_hash.py
+++ p3d_ssbo/algos/spatial_hash.py
@@ -91,3 +91,158 @@ class SpatialHash:
         cn.set_bounds_type(BoundingVolume.BT_box)
         cn.set_bounds(np.get_bounds())
         self.cnnp = cnnp
+
+
+pivot_start_source = """
+#version 430
+
+layout (local_size_x = 32, local_size_y = 1) in;
+
+{{ssbo}}
+
+void main() {
+  uint idx = uint(gl_GlobalInvocationID.x);
+  uint key = {{list_field}}[idx].{{list_key}};
+  uint diff;
+  if (idx == 0) {
+    // This is the first element with the lowest index, and thus we'll
+    // have to write it. So, how much further than the non-existant 
+    // index -1 are we?
+    diff = key + 1;
+  } else {
+    // A regular element. How many cell indices further are we than the
+    // last boid was? (If we're still in the same cell, that's 0.)
+    uint keyLeft = {{list_field}}[idx-1].{{list_key}};
+    diff = key - keyLeft;
+  }
+  // If this is such an cell-index-advancing element, we write its boid
+  // index into the pivot table as the start of a run. Since we may have
+  // advanced by *several* cell indices, we'll also need to set the
+  // start of the skipped ones; We set it to this element, so the
+  // runlength for them will be 0.
+  if (diff > 0) {
+    for (uint pivotIdx = key; pivotIdx > key - diff; pivotIdx--) {
+      {{table_field}}[pivotIdx].start = idx;
+    }
+  }
+  // If this is the last boid, we may need to set the start of any
+  // remaining cell indices, and we set them to "just beyond the end of
+  // the list of boids."
+  if (idx+1 == {{list_field}}.length()) {
+    for (uint pivotIdx = key + 1; pivotIdx < pivot.length(); pivotIdx++) {
+      {{table_field}}[pivotIdx].{{table_start}} = {{list_field}}.length();
+    }
+  }
+}
+"""[1:]
+
+
+pivot_length_source = """
+#version 430
+
+layout (local_size_x = 32, local_size_y = 1) in;
+
+{{ssbo}}
+
+void main() {
+  uint pivotIdx = uint(gl_GlobalInvocationID.x);
+  uint start = pivot[pivotIdx].start;
+  uint end;
+  if (pivotIdx == pivot.length() - 1) {
+    end = boids.length();
+  } else {
+    end = pivot[pivotIdx + 1].start;
+  }
+  pivot[pivotIdx].len = end - start;
+}
+"""[1:]
+
+
+class PivotTable:
+    def __init__(self, ssbo, key, table, debug=False):
+        self.ssbo = ssbo
+
+        list_field, list_key = key
+        list_struct = ssbo.get_field(list_field)
+        list_dims = list_struct.get_num_elements()
+        table_field, table_start, table_len = table
+        table_struct = ssbo.get_field(table_field)
+        table_dims = table_struct.get_num_elements()
+
+        # Start shader
+        render_args_start = dict(
+            ssbo=ssbo.full_glsl(),
+            list_field=list_field,
+            list_key=list_key,
+            table_field=table_field,
+            table_start=table_start,
+        )
+        template_start = Template(pivot_start_source)
+        source_start = template_start.render(**render_args_start)
+        if debug:
+            for line_nr, line_txt in enumerate(source_start.split('\n')):
+                print(f"{line_nr+1:4d}  {line_txt}")
+        shader_start = Shader.make_compute(Shader.SL_GLSL, source_start)
+        workgroups_start = (list_dims[0] // 32, 1, 1)
+        self.shader_start = shader_start
+        self.workgroups_start = workgroups_start
+
+        # Length shader
+        render_args_length = dict(
+            ssbo=ssbo.full_glsl(),
+        )
+        template_length = Template(pivot_length_source)
+        source_length = template_length.render(**render_args_length)
+        if debug:
+            for line_nr, line_txt in enumerate(source_length.split('\n')):
+                print(f"{line_nr+1:4d}  {line_txt}")
+        shader_length = Shader.make_compute(Shader.SL_GLSL, source_length)
+        workgroups_length = (table_dims[0] // 32, 1, 1)
+        self.shader_length = shader_length
+        self.workgroups_length = workgroups_length
+
+    def dispatch(self):
+        np = NodePath("dummy")
+        np.set_shader(self.shader_start)
+        np.set_shader_input(self.ssbo.glsl_type_name, self.ssbo.ssbo)
+        sattr = np.get_attrib(ShaderAttrib)
+        base.graphicsEngine.dispatch_compute(
+            self.workgroups_start,
+            sattr,
+            base.win.get_gsg(),
+        )
+
+        np = NodePath("dummy")
+        np.set_shader(self.shader_length)
+        np.set_shader_input(self.ssbo.glsl_type_name, self.ssbo.ssbo)
+        sattr = np.get_attrib(ShaderAttrib)
+        base.graphicsEngine.dispatch_compute(
+            self.workgroups_length,
+            sattr,
+            base.win.get_gsg(),
+        )
+
+    def attach(self, np, bin_name):
+        cn_s = ComputeNode(self.__class__.__name__ + "_start")
+        cn_s.add_dispatch(self.workgroups_start)
+        cnnp_s = np.attach_new_node(cn_s)
+
+        cnnp_s.set_shader(self.shader_start)
+        cnnp_s.set_shader_input(self.ssbo.glsl_type_name, self.ssbo.ssbo)
+
+        cnnp_s.set_bin(bin_name, 0)
+        cn_s.set_bounds_type(BoundingVolume.BT_box)
+        cn_s.set_bounds(np.get_bounds())
+        self.cnnp_s = cnnp_s
+
+        cn_l = ComputeNode(self.__class__.__name__ + "_length")
+        cn_l.add_dispatch(self.workgroups_length)
+        cnnp_l = np.attach_new_node(cn_l)
+
+        cnnp_l.set_shader(self.shader_length)
+        cnnp_l.set_shader_input(self.ssbo.glsl_type_name, self.ssbo.ssbo)
+
+        cnnp_l.set_bin(bin_name, 1)
+        cn_l.set_bounds_type(BoundingVolume.BT_box)
+        cn_l.set_bounds(np.get_bounds())
+        self.cnnp_l = cnnp_l
