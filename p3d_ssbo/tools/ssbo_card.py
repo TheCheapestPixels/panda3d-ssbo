5e47e4c246cab36acbbc312246d43339924c44aa
gossfunkel <gossfunkel@gmail.com>
2026-01-08 03:42:59 +0000

    tweaks to prepare for synchronised use with fft-ssbo

--- p3d_ssbo/tools/ssbo_card.py
+++ p3d_ssbo/tools/ssbo_card.py
@@ -44,21 +44,35 @@ void main() {
 
 
 class SSBOCard:
-    def __init__(self, parent, data_buffer, array_and_key):
-        array_name, key = array_and_key
-        render_args = dict(
-            ssbo=data_buffer.full_glsl(),
-            array=array_name,
-            key=key,
-        )
-        template = Template(fragment_template)
+    # this constructor takes either a name for the ssbo data
+    def __init__(self, parent: NodePath, data_buffer: gltypes.Buffer, *args):
+        if len(*args) < 2:
+            # buffer contains values
+            array_name = *args
+            render_args = dict(
+                ssbo=data_buffer.glsl(),
+                array=array_name,
+            )
+        elif len(*args) == 2:
+            array_name, key = *args
+            render_args = dict(
+                ssbo=data_buffer.full_glsl(),
+                array=array_name,
+                key=key,
+            )
+        else:
+            # this should really only have two overloads right?
+            # maybe in future this could handle multiple structs
+            raise Exception("SSBOCard *args should contain a name for the " + 
+                            "SSBO contents (str for values, iterable[str] for structs)")
+        template = template(fragment_template)
         fragment_source = template.render(**render_args)
-        vis_shader = Shader.make(
-            Shader.SL_GLSL,
+        vis_shader = shader.make(
+            shader.sl_glsl,
             vertex=vertex_source,
             fragment=fragment_source,
         )
-        cm = CardMaker('card')
+        cm = cardmaker('card')
         card = parent.attach_new_node(cm.generate())
         card.set_shader(vis_shader)
         card.set_shader_input(
